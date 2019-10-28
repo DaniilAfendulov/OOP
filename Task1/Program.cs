6df5a2873c9bb0eeacd347b7ac4b071aab3f5251
Daniil <37474366+DaniilAfendulov@users.noreply.github.com>
2019-10-28 15:49:15 +0300

    SearchSmallerDistance
    //В файле находятся координаты отрезков
    //Необходимо вывести на экран координаты тех отрезков,
    //длина которых меньше введенной

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿//В файле находятся координаты отрезков
+//Необходимо вывести на экран координаты тех отрезков,
+//длина которых меньше введенной
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchSmallerDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        struct Point
+        {
+            public double x, y;
+            public override string ToString()
+            {
+                return "(" + x + ";" + y + ")";
+            }
+        }
+
+        struct Vector
+        {
+            public Point startPoint, finishPoint;
+
+            public double Lenght()
+            {
+                return Math.Sqrt(Math.Pow((finishPoint.x - startPoint.x),2) 
+                    + Math.Pow((finishPoint.y - startPoint.y), 2));
+            }
+
+            public void PrintVectorPoints()
+            {
+                Console.WriteLine("Start point: " + startPoint.ToString() 
+                    + "\tFinish point: " + finishPoint.ToString());
+            }
+        }
+
+
+    }
+}
