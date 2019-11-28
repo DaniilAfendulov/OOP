d06ccac5e18e4df3e7ffbe1579438be52bbd34d7
Daniil <37474366+DaniilAfendulov@users.noreply.github.com>
2019-11-28 19:02:14 +0300

    CircleCrossing
    /*Задание
     * Задано 2 окружности. Окружность задается
     * центром и радиусом. Необходимо попределить
     * имеется ли пересечение. В случае, если есть 
     * пересечение, то вывести "да пересекается", 
     * если нет, то "нет пересечения"*/

---
+++ CircleCrossing/Circle.cs
@@ -0,0 +1,53 @@
+﻿//Афендулов Д. группа 1290
+/*Задание
+ * Задано 2 окружности. Окружность задается
+ * центром и радиусом. Необходимо попределить
+ * имеется ли пересечение. В случае, если есть 
+ * пересечение, то вывести "да пересекается", 
+ * если нет, то "нет пересечения"*/
+
+/*Сущности:
+ * Окружность(точка и радиус(число))
+ *     -пересекается ли с окружностью
+ *     -ввод окружности
+ * Точка(2 координаты(числа))
+ *     -ввод точки
+ *     -определение расстояния до точки
+ * Manager(2 окружности)
+ *     -Запрос о пересечении окружностей*/
+
+using System;
+
+namespace CircleCrossing
+{
+    public class Circle
+    {
+        Point _point;
+        double _radius;
+        public void ReadFromConsole()
+        {
+            _point = new Point();
+            _point.ReadFromConcole();
+
+            bool IsValid;
+            do
+            {
+                IsValid = false;
+                Console.Write("Введите радиус: ");
+                if (double.TryParse(Console.ReadLine(), out _radius) && _radius >= 0)
+                    IsValid = true;
+            } while (!IsValid);
+        }
+
+        public void IsCrossingCircle(Circle circle2)
+        {            
+
+            if (_point.FindDistanceForPoint(circle2._point) <= _radius + circle2._radius
+                    && _point.FindDistanceForPoint(circle2._point) >= Math.Abs(_radius - circle2._radius) )
+            {
+                Console.WriteLine("пересекаются");
+            }
+            else Console.WriteLine("не пересекаются");
+        }
+    }
+}
---
+++ CircleCrossing/Point.cs
@@ -0,0 +1,51 @@
+﻿//Афендулов Д. группа 1290
+/*Задание
+ * Задано 2 окружности. Окружность задается
+ * центром и радиусом. Необходимо попределить
+ * имеется ли пересечение. В случае, если есть 
+ * пересечение, то вывести "да пересекается", 
+ * если нет, то "нет пересечения"*/
+
+/*Сущности:
+ * Окружность(точка и радиус(число))
+ *     -пересекается ли с окружностью
+ *     -ввод окружности
+ * Точка(2 координаты(числа))
+ *     -ввод точки
+ *     -определение расстояния до точки
+ * Manager(2 окружности)
+ *     -Запрос о пересечении окружностей*/
+
+using System;
+
+namespace CircleCrossing
+{
+    public class Point
+    {
+        double _x, _y;
+        public void ReadFromConcole()
+        {
+            bool IsValid;
+            do
+            {
+                IsValid = true;
+                Console.WriteLine("Введите координаты центра окружности");
+
+                Console.Write("Введите x: ");
+                if (!double.TryParse(Console.ReadLine(), out _x))
+                    IsValid = false;
+
+                Console.Write("Введите y: ");
+                if (!double.TryParse(Console.ReadLine(), out _y))
+                    IsValid = false;
+
+
+            } while (!IsValid);
+        }
+
+        public double FindDistanceForPoint(Point point)
+        {
+            return Math.Sqrt(Math.Pow(_x - point._x, 2) + Math.Pow(_y - point._y, 2));
+        }
+    }
+}
